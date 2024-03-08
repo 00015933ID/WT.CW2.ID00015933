c39137f0fca79083645cd11068de8bd8424b7579
id00015933 <idd00015933@gmail.com>
2024-03-08 17:43:25 +0500

    changes of codes

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-mongoose.connect('mongodb+srv://Zarina:Zarina14@cluster0.wsz6gvy.mongodb.net/?retryWrites=true&w=majority')
 
-
-app.set('view engine', "ejs")
+app.set('view engine', 'ejs')
 
 app.use('/articles', articleRouter)
-app.use(express.urlencoded({ extended: false}))
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     const articles = [{
-        title: 'Test Article',
-        createdAt: new Date,
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date,
-        description: 'Test description 2'
+        title: "Test Article",
+        createdAt: Date.now(),
+        description: "Test Description"
 
     }]
-    res.render('articles/index', { articles: articles })
+    res.render('index', { articles: articles })
 })
+
 
 app.listen(5000)
